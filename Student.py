f2d343ba50dd8b5f23ad978956b05454a545e621
toucanwhale <136608688+toucanwhale@users.noreply.github.com>
2023-06-27 09:53:30 -0500

    Update Student.py

--- Student.py
+++ Student.py
@@ -1,36 +1,68 @@
-class Student():
-    def __init__(self, first_name, last_name, major, credit_hours, gpa, id):
+#Create a student Class with these attributes
+#First name
+#last name
+#major
+#credit hours (freshman, sophomore, junior, senior)
+#gpa
+#ID number
+#create get/set methods where appropriate
+class Student:
+    def __init__(self, first_name, last_name, major, credit_hours, gpa, ID):
         self.__first_name = first_name
         self.__last_name = last_name
         self.__major = major
         self.__credit_hours = credit_hours
         self.__gpa = gpa
-        self.__id = id
+        self.__ID = ID
+        #self.__class_level = sel.
     
-    def get_student_data(self, first_name, last_name, major, credit_hours, gpa, id):
-        return self.__first_name, self.__last_name, self.__major, self.__credit_hours, self.__gpa, self.__id
+    def get_first_name(self):
+        return self.__first_name
+    
+    def set_first_name(self, new_fname):
+        self.__first_name = new_fname
+
+    def get_last_name(self):
+        return self.__last_name
+    
+    def set_last_name(self, new_lname):
+        self.__last_name = new_lname
+
+    def get_major(self):
+        return self.__major
     
     def set_major(self, new_major):
         self.__major = new_major
 
-    def set_credit_hours(self, additional_hours):
-        self.__credit_hours += additional_hours
+    def get_credit_hours(self):
+        return self.__credit_hours
 
-    def get_class_level(self):
-        if self.__credit_hours>=0 and self.__credit_hours<=30:
-            return "Freshman"
-        elif self.__credit_hours>30 and self.__credit_hours<=60:
-            return "Sophomore"
-        elif self.__credit_hours>60 and self.__credit_hours<=90:
-            return "Junior"
-        elif self.__credit_hours>90:
-            return "Senior"
-        else:
-            return "Error"
-        
+    def set_credit_hours(self, new_credits):
+        self.__credit_hours = new_credits
+    
+    def update_credit_hours(self, new_credits):
+        self.__credit_hours += new_credits
+    
+    def get_gpa(self):
+        return self.__gpa
+    
     def set_gpa(self, new_gpa):
         self.__gpa = new_gpa
+    
+    def get_ID(self):
+        return self.__ID
 
-
+    def get_class_level(self):
+        if self.__credit_hours > 90:
+            return "Senior"
+        elif self.__credit_hours > 60:
+            return "Junior"
+        elif self.__credit_hours > 30:
+            return "Sophomore"
+        else:
+            return "Freshman"
+    
     def print_student_data(self):
-        print(f"Name: {self.__first_name} {self.__last_name}. Major: {self.__major}. Credit hours: {self.__credit_hours}. GPA: {self.__gpa}. ID#: {self.__id}")
+        print(f"{self.__first_name} {self.__last_name}")
+        print(f"Class Level: {self.get_class_level()}, Major: {self.__major}")
+        print(f"GPA: {self.__gpa}, ID: {self.__ID}\n")
